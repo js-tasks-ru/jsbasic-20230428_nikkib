0e720924634b54f1d106e2966bbd7f63132dad61
Veronica Buravkova <veronica@webmercs.com>
2023-05-03 13:12:10 +0400

    1-module 1-task

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,16 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    
+    let result = 1;
+    return result;
+
+  } else {
+    let result = n;
+
+    for (let i = 1; i <= n-1; i++) {
+      result *= n - i;
+    }
+
+    return result;
+  }
 }
