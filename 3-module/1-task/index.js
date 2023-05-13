0c4250dd7e5652f82a1f6e445b3a1b7cffac29ac
Veronica Buravkova <veronica@webmercs.com>
2023-05-13 22:25:08 +0400

    3-module 1-task

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,14 @@
 function namify(users) {
-  // ваш код...
+  // Вариант 1 - методом массива map
+  //  return users.map(user => user.name);
+
+  //Вариант 2 - с циклом
+  let names = [];
+  
+  for (let user of users) {
+    names.push(user.name); 
+  }
+  
+  return names;
 }
+
