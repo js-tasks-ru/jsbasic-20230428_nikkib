4cb44466682e0cb18e60d795380ffea55af49fb9
Veronica Buravkova <veronica@webmercs.com>
2023-05-14 20:38:57 +0400

    3-module 4-task

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,16 @@
 function showSalary(users, age) {
-  // ваш код...
+  let usersFiltered = users
+                        .filter(user => (user.age <= age))
+                        .map(user => ({name: user.name, balance: user.balance}));
+
+  let strUsersSalary = '';
+  for (let i = 0; i < usersFiltered.length; i++) {
+    if (i == usersFiltered.length - 1) {
+      strUsersSalary = strUsersSalary + usersFiltered[i].name + ', ' + usersFiltered[i].balance;
+    } else {
+      strUsersSalary = strUsersSalary + usersFiltered[i].name + ', ' + usersFiltered[i].balance + '\n'
+    };
+  }
+
+  return strUsersSalary;
 }
