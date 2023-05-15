0dbaaa5f79f02fac9b77f1193da46da0b5e6387b
Veronica Buravkova <veronica@webmercs.com>
2023-05-15 17:26:10 +0400

    3-module 4-task fix review notes

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,16 +1,6 @@
 function showSalary(users, age) {
-  let usersFiltered = users
-                        .filter(user => (user.age <= age))
-                        .map(user => ({name: user.name, balance: user.balance}));
-
-  let strUsersSalary = '';
-  for (let i = 0; i < usersFiltered.length; i++) {
-    if (i == usersFiltered.length - 1) {
-      strUsersSalary = strUsersSalary + usersFiltered[i].name + ', ' + usersFiltered[i].balance;
-    } else {
-      strUsersSalary = strUsersSalary + usersFiltered[i].name + ', ' + usersFiltered[i].balance + '\n'
-    };
-  }
-
-  return strUsersSalary;
+  return users
+          .filter(user => (user.age <= age))
+          .map(user => user.name + ', ' + user.balance)
+          .join('\n');
 }
