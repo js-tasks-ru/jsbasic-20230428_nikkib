b233d5921d25270f10bcc005369acc27b8a3c393
Veronica Buravkova <veronica@webmercs.com>
2023-05-18 12:02:35 +0400

    4-module 3-task

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,27 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i ++) {
+    let row = table.rows[i];
+
+    if (row.lastElementChild.hasAttribute('data-available')) {
+      let status = row.lastElementChild.getAttribute('data-available');
+      
+      if (status == 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
+    } else {
+      row.setAttribute('hidden', 'true');
+    }
+
+    if (row.cells[2].textContent == 'm') {
+      row.classList.add('male');
+    } else if (row.cells[2].textContent == 'f') {
+      row.classList.add('female');
+    }
+
+    if (row.getElementsByTagName('td')[1].textContent < 18) {
+      row.style = "text-decoration: line-through";
+    }
+  }
 }
