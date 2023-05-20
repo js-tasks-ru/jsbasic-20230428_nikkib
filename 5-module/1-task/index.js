4c0ae8137b9117717e0e9c69bc316b0cf907f439
Veronica Buravkova <veronica@webmercs.com>
2023-05-20 17:34:42 +0400

    5-module 1-task

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,16 @@
+/*Я совсем не поняла это задание - почему в index.html сразу стоит   
+  <script>
+    hideSelf();
+  </script>
+  Получается, мы сразу же и вызываем эту функцию?
+  
+  И тесты не проходили, если пытаться в index.js навесить обработчик события 
+    button.addEventListener('click', hideSelf, {once: true}); или
+    button.onclick=hideSelf; 
+    на кнопку, пришлось его в index.html прописывать
+*/
+
 function hideSelf() {
-  // ваш код...
+  let button = document.getElementsByClassName('hide-self-button')[0];
+  button.hidden = true;
 }
