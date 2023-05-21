1a9fe4bb558e89f8226c13ac6886be7ac5c77698
Veronica Buravkova <veronica@webmercs.com>
2023-05-21 08:31:59 +0400

    5-module 2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  document.addEventListener('click', function(event) {
+    let elem = document.getElementById("text");
+
+    if (event.target.classList.contains("toggle-text-button")) {
+      elem.hidden = !elem.hidden;
+    }
+  });
 }
