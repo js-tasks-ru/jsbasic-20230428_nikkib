921e2024e68ee2c0b3bd40d4156d06111e45c8ca
Veronica Buravkova <veronica@webmercs.com>
2023-05-21 16:19:00 +0400

    5-module 3-task

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,34 @@
 function initCarousel() {
-  // ваш код...
+  let carouselArrowRight = document.querySelector('.carousel__arrow_right');
+  let carouselArrowLeft = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let carouselSlideWidth = carouselInner.querySelector('img').offsetWidth;
+  let translatePosition = 0;
+  let slidePosition = 0;
+
+  carouselArrowLeft.style.display = 'none';
+
+  document.addEventListener('click', function(event) {
+
+    if (event.target == carouselArrowRight) {
+      translatePosition += carouselSlideWidth;
+      carouselInner.style.transform = 'translateX(-' + translatePosition +'px)';
+      slidePosition++;
+    }
+    
+    if (event.target == carouselArrowLeft) {
+      translatePosition -= carouselSlideWidth;
+      carouselInner.style.transform = 'translateX(-' + translatePosition +'px)';
+      slidePosition--;
+    };
+
+    if (slidePosition == 0) {
+      carouselArrowLeft.style.display = 'none';
+    } else if (slidePosition == 3) {
+      carouselArrowRight.style.display = 'none';
+    } else {
+      carouselArrowLeft.style.display = '';
+      carouselArrowRight.style.display = '';
+    }
+  });
 }
