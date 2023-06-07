3c0c65ea2a0f8aac0d27b28db26c7ef9a0e3580c
Veronica Buravkova <veronica@webmercs.com>
2023-06-07 11:01:06 +0400

    9 module 1,2 tasks

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,5 @@
 export default function promiseClick(button) {
-  // ваш код...
+  return new Promise(function(resolve) {
+    button.addEventListener('click', (event) => { resolve(event) }, { once: true });
+  });
 }
